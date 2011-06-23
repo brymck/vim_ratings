0ccc321dac18a5626c184122959c48d69586d09b
Bryan McKelvey <bryan.mckelvey@gmail.com>
2011-06-23 09:34:37 +0900

    Maybe things work now

--- install/windows.bat
+++ install/windows.bat
@@ -1,17 +1,20 @@
 @echo off
-echo Packing extension into .crx ...
-cd vim_ratings
-"%AppData%\..\Local\Google\Chrome\Application\chrome.exe" --pack-extension="%cd%\src" --pack-extension-key="%cd%\vimratings.pem" --no-message-box
+echo Packing into Chrome extension ...
 set counter=0
+if exist vim_ratings.pem (
+  "%AppData%\..\Local\Google\Chrome\Application\chrome.exe" --pack-extension="%cd%\src" --pack-extension-key="%cd%\vim_ratings.pem" --no-message-box
+  goto test
+)
+"%AppData%\..\Local\Google\Chrome\Application\chrome.exe" --pack-extension="%cd%\src" --no-message-box
 
 :test
-if exist "src.crx" goto finish
+if exist src.crx goto finish
 set /a counter+=1
 if %counter%==10 goto error
 goto wait
 
 :wait
-ping 127.0.0.1 -n 1 -w 1000 > nul
+ping 1.1.1.1 -n 1 -w 1000 > nul
 goto test
 
 :error
@@ -19,8 +22,15 @@ echo Error installing!
 goto exit
 
 :finish
-copy /y src.crx vimratings.crx
+echo Renaming Chrome extension and private key...
+copy /y src.crx vim_ratings.crx > nul
 del src.crx
+if exist src.pem (
+  copy /y src.pem vim_ratings.pem > nul
+  del src.pem
+)
+
+echo Opening in Chrome...
 "%AppData%\..\Local\Google\Chrome\Application\chrome.exe" "%cd%\vimratings.crx"
 echo Done!
 
