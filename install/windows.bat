1260a074885f4230e95e54d4ce16de948f88acac
Bryan McKelvey <bryan.mckelvey@gmail.com>
2011-06-23 09:11:21 +0900

    More stuff

--- install/windows.bat
+++ install/windows.bat
@@ -2,17 +2,26 @@
 echo Packing extension into .crx ...
 cd vim_ratings
 "%AppData%\..\Local\Google\Chrome\Application\chrome.exe" --pack-extension="%cd%\src" --pack-extension-key="%cd%\vimratings.pem" --no-message-box
+set counter=0
 
 :test
 if exist "src.crx" goto finish
-else goto wait
+set /a counter+=1
+if counter==10 goto error
+goto wait
 
 :wait
 ping 127.0.0.1 -n 1
 goto test
+
+:error
+echo Error installing!
+goto exit
 
 :finish
 copy /y src.crx vimratings.crx
 del src.crx
 "%AppData%\..\Local\Google\Chrome\Application\chrome.exe" "%cd%\vimratings.crx"
 echo Done!
+
+:exit
